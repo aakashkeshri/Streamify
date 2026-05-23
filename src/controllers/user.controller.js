9af6320d29c9ea8f4cb2aa7fec222d1da39a5d46
Aakash Keshri <keshriaakash2006@gmail.com>
2026-05-23 14:17:20 +0530

    refresh token controllers and update routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import {ApiError} from '../utils/apiError.js';
 import {User} from '../models/user.model.js';
 import{uploadOnCloudinary} from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
-
+import jwt from 'jsonwebtoken';
 
 const generateAccessAndRefreshToken=async(userId)=>{
     try{
@@ -161,7 +161,8 @@ const logoutUser= asyncHandler(async(req,res)=>{
         {
             $set:{refreshToken:""}
         },
-        {new:true},
+        // old method{new:true},
+        {returnDocument:"after"} // new method to return updated document
 
     )
 
@@ -185,8 +186,60 @@ const logoutUser= asyncHandler(async(req,res)=>{
     
 })
 
+
+const refreshAccessToken= asyncHandler(async(req,res)=>{
+    const incomingRefreshToken = req.cookies?.refreshToken || req.body?.refreshToken 
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized request, refresh token is missing");
+    }
+
+    try{
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id);
+
+        if(!user){
+            throw new ApiError(401, "Invalid refresh token, user not found");
+        }
+
+        if(user?.refreshToken !== incomingRefreshToken){
+            throw new ApiError(401, "Refresh token is expired or invalid");
+        }
+
+        const options={
+            httpOnly:true,
+            secure:true
+        }
+
+        const {accessToken, newrefreshToken} = await generateAccessAndRefreshToken(user._id);
+
+        return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", newrefreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    accessToken,
+                    newrefreshToken
+                },
+                "Access token refreshed successfully"
+            )
+        );
+        
+    }
+
+    catch(error){
+        throw new ApiError(401, error?.message || "Invalid refresh token");
+    }
+
+})
+
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import {asyncHandler} from "express-async-handler"
+import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/apiError.js";
 import jwt from "jsonwebtoken";
 import {User} from "../models/user.model.js";
